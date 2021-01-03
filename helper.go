b3d37d0c6805ec3a95f35863966352be99dfb391
atul045 <atul@appointy.com>
2021-01-03 09:24:08 +0530

    MakeDirectory method added

--- helper.go
+++ helper.go
@@ -1,7 +1,12 @@
 package helper
 
 import (
+	"errors"
+	"fmt"
 	"math"
+	"os"
+	"runtime"
+	"strings"
 )
 
 type Container struct {
@@ -28,10 +33,10 @@ func (c *Container) Power() float64 {
 	return result
 }
 
-// trim float function trim the float no with certain digits
+// Trim float function trim the float number with certain digits
 // in case the value of digit is zero or negative then we will return
-//the actual number
-// i.e := if no is 123.3454 and digit is 2 then result will be
+// the actual number
+// i.e := if number is 123.3454 and digit is 2 then result will be
 // 123.34
 func TrimFloat(no float64, digit int64) float64 {
 	if digit <= 0 {
@@ -39,4 +44,48 @@ func TrimFloat(no float64, digit int64) float64 {
 	}
 	d := math.Pow(10, float64(digit))
 	return float64(int64(no*d)) / d
+}
+
+
+type Directory struct {
+	Override bool
+	Name string
+	Path string
+	Permission int64
+}
+
+// Make Directory is a receiver function which
+// will create a directory to the specified path.
+// If we set override true then it will delete the specified folder and create new
+// directory.
+// Default Permission of the created directory will be 0755.
+//
+func (dir *Directory) MakeDirectory() error {
+    filepath := dir.Path+"/"+dir.Name
+	//Checking the operating system.
+	if runtime.GOOS == "windows" {
+		filepath = dir.Path+"\\"+dir.Name
+	}
+
+	if dir.Override {
+		if err := os.RemoveAll(filepath); err != nil {
+			return err
+		}
+	}
+
+	_, err := os.Stat(filepath)
+	if os.IsNotExist(err) {
+		errDir := os.MkdirAll(filepath, os.FileMode(dir.Permission))
+		if errDir != nil {
+          return errDir
+		}
+	}
+	return nil
+}
+
+func defaultDirectory() *Directory {
+	return &Directory{
+		Override:   false,
+		Permission: 0755,
+	}
 }
